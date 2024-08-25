49808615243758fe5286907998bfe6441d54699d
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2024-08-25 21:20:09 +1000

    Added comments

--- motor/motor.cpp
+++ motor/motor.cpp
@@ -1,10 +1,5 @@
 #include "motor.h"
 
-Motor::Motor(Servo &l_servo, Servo &r_servo){
-  this-> r_servo = r_servo;
-  this-> l_servo = l_servo;
-}
-
 Motor::Motor(Servo &l_servo, Servo &r_servo, unsigned long runtime){
   this-> r_servo = r_servo;
   this-> l_servo = l_servo;
@@ -12,104 +7,124 @@ Motor::Motor(Servo &l_servo, Servo &r_servo, unsigned long runtime){
 }
 
 void Motor::init(byte l_pin, byte r_pin){
-  l_servo.attach(l_pin);
-  r_servo.attach(r_pin);
+  l_servo.attach(l_pin, 700, 2300);
+  r_servo.attach(r_pin, 700, 2300);
 }
 
-void Motor::forward(int speed){
+void Motor::forward(){
+  //start timer
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  int l_speed = set_l_speed(speed);
-  int r_speed = set_r_speed(speed);
-  while (timeNow - start < runtime){
+  //assign speeds to each motor
+  int l_speed = set_l_speed(20);
+  int r_speed = set_r_speed(20);
+  //moves forward until runtime ends.
+  if (timeNow - start < runtime){
     l_servo.writeMicroseconds(l_speed);
     r_servo.writeMicroseconds(r_speed);
-    delay(10);
+    delay(100);
     timeNow = millis();
   }
 }
 
-void Motor::backward(int speed){
+void Motor::backward(){
+  //start timer
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  int l_speed = abs(set_l_speed(speed)-1500) + 1500;
-  int r_speed = abs(set_l_speed(speed)-1500) + 1500;
-  while (timeNow - start < runtime){
-    l_servo.writeMicroseconds(l_max_counterclockwise);
-    r_servo.writeMicroseconds(r_max_counterclockwise);
-    delay(50);
+  //the reverse speed is the same distance away from the midpoint as the forward speed
+  int l_speed = abs(set_l_speed(15)-1500)+1500;
+  int r_speed = abs(set_r_speed(15)-1500)+1500;
+  if (timeNow - start < runtime){
+    l_servo.writeMicroseconds(l_speed);
+    r_servo.writeMicroseconds(r_speed);
+    delay(100);
     timeNow = millis();
   }
 }
 
 void Motor::stop_motor(){
+  //stop both motors
   unsigned long timeNow = millis();
   unsigned long start = millis();
-  while (timeNow - start < runtime){
+  if (timeNow - start < runtime){
     l_servo.writeMicroseconds(1500);
     r_servo.writeMicroseconds(1500);
+    delay(100);
     timeNow = millis();
   }
 }
 
 void Motor::turn_left(){
+  //turn 90 degress left
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  while (timeNow - start < turn_90){
+  if (timeNow - start < turn_90){
     l_servo.writeMicroseconds(700);
     r_servo.writeMicroseconds(700);
-    delay(50);
+    delay(100);
     timeNow = millis();
   }
 }
 
-void Motor::adjust_left(int r_speed, int l_speed){
+void Motor::adjust_left(int r_speed){
+  //slightly swivel to the left. intensity is specified using r_speed.
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  while (timeNow - start < runtime){
-    l_servo.writeMicroseconds(l_speed);
+  r_speed = set_r_speed(r_speed);
+  if (timeNow - start < runtime){
+    l_servo.writeMicroseconds(1500);
     r_servo.writeMicroseconds(r_speed);
-    delay(50);
+    delay(100);
     timeNow = millis();
   }
 }
 
 void Motor::turn_right(){
+  //turn 90 degress right
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  while (timeNow - start < turn_90){
+  if (timeNow - start < turn_90){
     l_servo.writeMicroseconds(2300);
     r_servo.writeMicroseconds(2300);
-    delay(50);
+    delay(100);
     timeNow = millis();
   }
 }  
 
-void Motor::adjust_right(int r_speed, int l_speed){
+void Motor::adjust_right(int l_speed){
+  //slightly swivel to the right. intensity is specified using l_speed
   unsigned long start = millis();
   unsigned long timeNow = millis();
-  while (timeNow - start < runtime){
-    l_servo.writeMicroseconds(1500);
+  l_speed = set_l_speed(l_speed);
+  if (timeNow - start < runtime){
+    l_servo.writeMicroseconds(l_speed);
     r_servo.writeMicroseconds(1500);
-    delay(50);
+    delay(100);
     timeNow = millis();
   }
 } 
 
-void Motor::test(){
-  r_servo.writeMicroseconds(1440);
-  l_servo.writeMicroseconds(1560);
-  delay(500);
-}
+
 
 int Motor::set_r_speed(int percent){
+  //set speed for the right motor
   int val = 0;
   val = static_cast<int>(map(percent, 0, 100, 1500, 700));
   return val;
 }
 
 int Motor::set_l_speed(int percent){
+  //set speed for the left motor
   int val = 0;
   val = static_cast<int>(map(percent, 0, 100, 1500, 2300));
   return val;
+}
+
+void Motor::test(){
+  Serial.println("Test running.");
+  int l_speed = set_l_speed(15);
+  int r_speed = set_r_speed(10);
+  l_servo.writeMicroseconds(l_speed);
+  r_servo.writeMicroseconds(r_speed);
+  delay(100);
 }
--- motor/motor.h
+++ motor/motor.h
@@ -8,29 +8,20 @@ class Motor{
     Servo r_servo;
     byte l_pin;
     byte r_pin;
-    const int l_stop = 1500;
-    const int l_max_clockwise = 2300;
-    const int l_max_counterclockwise = 700;
-    const int turn_90 = 520;
-    const int r_stop = 1500;
-    const int r_max_clockwise = 700;
-    const int r_max_counterclockwise = 2300;
     unsigned long runtime;
   public:
     Motor(){}
-    Motor(Servo &l_servo, Servo &r_servo);
     Motor(Servo &l_servo, Servo &r_servo, unsigned long runtime);
     void init(byte l_pin, byte r_pin);
     void stop_motor();
-    void forward(int speed);
-    void backward(int speed);
+    void forward();
+    void backward();
     int set_r_speed(int percent);
     int set_l_speed(int percent);
     void turn_left();
     void turn_right();
     void test();
-    void adjust_left(int r_speed, int l_speed);
-    void adjust_right(int r_speed, int l_speed);
+    void adjust_left(int r_speed);
+    void adjust_right(int l_speed);
 };
-
 #endif
