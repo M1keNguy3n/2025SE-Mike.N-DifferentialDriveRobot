2fc918c2999970acbe55e66ad19e67cb4894327b
M1keNguy3n <mike.nguyen696@education.nsw.gov.au>
2024-08-25 21:21:05 +1000

    IR Class

---
+++ ir.cpp
@@ -0,0 +1,54 @@
+#include "ir.h"
+
+IR::IR(byte pin_digital, byte pin_analog){
+  //instantiate an IR sensor with digital and analog pin
+  this -> pin_digital = pin_digital;
+}
+
+IR::IR(byte pin_analog){
+  //instantiate an IR sensor with analog pin
+  this -> pin_analog = pin_analog;
+}
+
+void IR::init(){
+  //initialize the analog and digital pin, if available.
+  pinMode(pin_analog, INPUT);
+  state_analog = analogRead(pin_analog);
+  pinMode(pin_digital, INPUT);
+  state_digital = digitalRead(pin_digital);
+}
+
+void IR::init_digital(){
+  //initialize the digital pin, if available.
+  pinMode(pin_digital, INPUT);
+  state_digital = digitalRead(pin_digital);
+}
+
+void IR::init_analog(){
+  //initialize the analog pin, if available.
+  pinMode(pin_analog, INPUT);
+  state_analog = analogRead(pin_analog);
+}
+
+byte IR::readStateDigital(){
+  //return a byte value, 0 or 1.
+  state_digital = digitalRead(pin_digital);
+  return state_digital;
+}
+
+unsigned long IR::readStateAnalog(){
+  //returns the value from the ir.
+  delay(10);
+  return analogRead(pin_analog);
+}
+
+bool IR::l_lineDetected(){
+  //if the value is between 58 and 65, return true.
+  return (readStateAnalog() < 46);
+}
+
+bool IR::r_lineDetected(){
+  //if the value is between 46 and 56, return true.
+  return (readStateAnalog() < 46);
+}
+
---
+++ ir.h
@@ -0,0 +1,22 @@
+#ifndef IR_H
+#define IR_H
+#include <Arduino.h>
+class IR{
+  private:
+    byte state_digital;
+    byte pin_digital;
+    byte pin_analog;
+    unsigned long state_analog;
+  public:
+    IR(){} //do not use
+    IR(byte pin_analog);
+    IR(byte pin_digital, byte pin_analog);
+    void init_digital();
+    void init_analog();
+    void init();
+    byte readStateDigital();
+    unsigned long readStateAnalog();
+    bool l_lineDetected();
+    bool r_lineDetected();
+};
+#endif
